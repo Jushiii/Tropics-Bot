b4e782484cf346003885e2f66d696d50824ad83a
Jushiii <Jushiii@users.noreply.github.com>
2017-03-31 15:08:46 -0700

    8ball, and flip added, new server members in works

--- bot.js
+++ bot.js
@@ -18,7 +18,14 @@ client.on("message", message => {
         message.channel.sendMessage(`That cool pineapple kid? Nice taste, ${message.author.username}.`)
     }
     if(commandIs("help", message)){
-        message.channel.sendMessage('Current commands: !hello, !aidan')
+        message.channel.sendMessage('Current commands: !hello, !aidan')    
+    }
+    if(commandIs(`info`, message)){
+        message.channel.sendMessage(`Ok, ${message.author.username}, here's information about me:`);
+        message.channel.sendMessage(`Library | :books::**Discord.js`);
+        message.channel.sendMessage(`Developer | :bust_in_silhouette::**Fruit**`);
+        message.channel.sendMessage(`Version | :gear::**Currently INDEV**`);
+        message.channel.sendMessage(`Planned Features | :soon::**Music, dice rolling, streaming notifications and many more!**`);
     }
 });
 
